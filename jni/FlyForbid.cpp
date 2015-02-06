8abb3080d058a8d4302d3a8d52dd1c4f043ab6ab
Kevin Finisterre <kf@digitalmunition.com>
2015-02-06 10:45:14 -0500

    attempt to fill in the proper JNI constructs

--- jni/FlyForbid.cpp
+++ jni/FlyForbid.cpp
@@ -1,15 +1,38 @@
 #include <string.h>
 #include <jni.h>
-//extern "C"
-//{
-//	JNIEXPORT jstring JNICALL Java_com_example_Hellojnicpp_stringFromJNI(JNIEnv *env, jobject obj)
-//	{
-//            return env->NewStringUTF("Hello from C++ over JNI!");
-//	}
-//}
 
-//  every single native method takes as its first two arguments "JNIEnv* aEnv" and  "jobject aObj". These arguments are not part of method signature in registration table but you have to include them.
-void engine_stop(JNIEnv* aEnv, jobject aObj, jboolean aTerminating)
+/*
+
+From the binary strings:
+
+dji/midware/natives/FlyForbid$FlyForbidParam
+count
+SetForbidPoint
+([D[D[DI)V
+dji/midware/natives/FlyForbid
+native_CheckNearForbidPoints
+(DDLjava/lang/Object;)Ldji/midware/natives/FlyForbid$FlyForbidParam;
+native_intersectSegCircle
+(DDDI)Z
+
+From the onLoad() errors:
+
+Ldji/midware/natives/FlyForbid;.native_CheckNearForbidPoints:(DDLjava/lang/Object;)Ldji/midware/natives/FlyForbid$FlyForbidParam;
+
+*/
+
+//  every single native method takes as its first two arguments "JNIEnv* aEnv" and  "jobject aObj". These arguments are not 
+// part of method signature in registration table but you have to include them.
+
+void native_CheckNearForbidPoints(JNIEnv* aEnv, jobject aObj, jboolean forbidden)
+{
+}
+
+void native_intersectSegCircle(JNIEnv* aEnv, jobject aObj, jboolean intersects)
+{
+}
+
+void SetForbitPoint(JNIEnv* aEnv, jobject aObj, jboolean intersects)
 {
 }
 
@@ -17,12 +40,12 @@ extern "C"
 {
     JavaVM* gJavaVM = NULL;
     jobject gJavaActivityClass;
-    const char* kJavActivityClassPath = "com/sbcgames/sbcengine/SBCEngine";
+    const char* kJavActivityClassPath = "dji/midware/natives/FlyForbid";
     
     static JNINativeMethod methodTable[] = {
-        {"engine_tick", "()V", (void *) engine_stop},
-        {"engine_start", "(Landroid/content/res/AssetManager;)V", (void *) engine_stop},
-        {"engine_stop", "(Z)V", (void *) engine_stop},
+        {"SetForbitPoint", "([D[D[DI)V", (void *) SetForbitPoint},
+        {"native_intersectSegCircle", "(DDDI)Z", (void *) native_intersectSegCircle},
+        {"native_CheckNearForbidPoints", "(DDLjava/lang/Object;)Ldji/midware/natives/FlyForbid$FlyForbidParam;", (void *) native_CheckNearForbidPoints},
        };
     
     //------------------------------------------------------------------------
